58195241d02cf19ee71a69791e81e9d8504e1626
Levi Willrich <levijwillrich@gmail.com>
2018-07-02 11:38:49 -0500

    fixed robots

--- modules/robots.py
+++ modules/robots.py
@@ -3,10 +3,10 @@ import urllib2,urlparse
 class Robots:
     def __init__(self):
         self.runonce = True
-        self.name="Robots"
+        self.name="robots"
     def parse_page(self,page_data,page_url):
         try:
-            return urllib2.open(urlparse.urljoin(page_url,robots.txt))
+           return urllib2.urlopen(urlparse.urljoin(page_url,"robots.txt")).read()
         except Exception:
-            return "NO_ROBOTS_TXT"
-self.modules=Robots()
+           return "NO_ROBOTS_TXT"
+module=Robots()
