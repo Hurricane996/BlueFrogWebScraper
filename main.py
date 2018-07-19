2a77a5748a0a4d92623f46ea537811f437d073a5
Levi Willrich <levijwillrich@gmail.com>
2018-07-19 09:38:41 -0500

    Started work on main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python
 
+import argparse
+
+parser = argparse.ArgumentParser(description="tool that gets seo-related information by crawling website")
+
+parser.add_argument("site",help="the website you want to run the scraper on")
+parser.add_argument("modules",help="the modules you wish to load, or all for all of them",default="all",nargs="*");
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-p","--page",help="run the scraper on a single page instead of an entire site")
+group.add_argument("-s","--sitemap",help="specify a custom sitemap location",default="sitemap.xml")
+
+args=parser.parse_args()
+
+print args.modules
